205205cae5ca0238687f5c41585713aaa38e1067
americano- <none>
2014-06-10 08:01:34 -0700

    short searh key

--- bing-search.py
+++ bing-search.py
@@ -4,6 +4,8 @@ from time import sleep
 import urllib2
 import random
 import sys
+import json
+
 username = sys.argv[1]
 password = sys.argv[2]
 try: 
@@ -16,8 +18,10 @@ if "mobile" in is_mobile.lower():
     fp = webdriver.FirefoxProfile()
     fp.set_preference("general.useragent.override", 'User Agent String :: mozilla/5.0 (iphone; cpu iphone os 7_0_2 like mac os x) applewebkit/537.51.1 (khtml, like gecko) version/7.0 mobile/11a501 safari/9537.53')
     driver = webdriver.Firefox(firefox_profile=fp)
+    num_searches = 20
 else:
     driver = webdriver.Firefox()
+    num_searches = 30
 
 driver.get("http://www.outlook.com")
 elem = driver.find_element_by_name("login")
@@ -29,11 +33,12 @@ assert "Outlook" in driver.title
 print "logged in as: " + username
 
 # start searching
-url = 'http://www.iheartquotes.com/api/v1/random'
-for i in range(30):
+url = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+
+for i in range(num_searches):
     print username, "(", i, ")"
-    query = urllib2.urlopen(url).read()
-    driver.get("http://www.bing.com/search?q=" + query)
-    sleep(random.randint(1,2))
+    res = json.load(urllib2.urlopen(url))
+    driver.get("http://www.bing.com/search?q=" + res['word'])
+    sleep(random.randint(2,5))
 driver.close()
 
